db38a34235a49c34e9b1a12d8e402b48907c7a14
kmcode <kmcode@kmpc>
2025-05-06 16:19:45 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import DroneDashboard from './ui/components/organisms/DroneDashboard';
 function App() {
     return (
       <div className="App bg-gray-50 min-h-screen">
+        <h1 className="text-xl font-bold text-center mt-10">Frontend Coding Challenge</h1>;
         <DroneDashboard />
       </div>
     );
